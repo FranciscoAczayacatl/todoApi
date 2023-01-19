25b53acf18d131d66724ad9a21d1c8457d4fe81e
FranciscoAczayacatl <franciscoaczayacatlg@gmail.com>
2023-01-18 19:16:52 -0600

    'fix-token'

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,24 +5,34 @@ require('dotenv').config();
 
 const authMiddleware = (req, res, next) => {
   let { authorization: token } = req.headers;
-  token = token.replace("Bearer ", "");
+  token = token?.replace("Bearer ", "");
   console.log(token);
-  jwt.verify(
-    token,
-    process.env.JWT_SECRET,
-    { algorithms: "HS512" },
-    (err, decoded) => {
-      if (err) {
-        res.status(400).json({
-          error: "invalid token",
-          message: "El token no es válido o ya expiro, envía un token correcto",
-        });
-      } else {
-        console.log(decoded);
-        next();
+  if(token){
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET,
+      { algorithms: "HS512" },
+      (err, decoded) => {
+        if (err) {
+          res.status(400).json({
+            error: "invalid token",
+            message: "El token no es válido o ya expiro, envía un token correcto",
+          });
+        } else {
+          console.log(decoded);
+          next();
+        }
       }
-    }
-  );
+    );
+  }else {
+    res.status(400).json(
+      {
+        error: "no token provided",
+        message: "no se esta enviando un token de autenticacion"
+      }
+    )
+  }
+  
 };
 
 module.exports = authMiddleware;
